6caadd82b46362f24f84463d1da5080b84c31c41
Hugh McKee <mckee.hugh@gmail.com>
2018-01-26 08:17:31 -0500

    refactor test

--- src/test/java/sudokugen/GridTest.java
+++ src/test/java/sudokugen/GridTest.java
@@ -8,9 +8,9 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class GridTest {
     @Test
     public void thatSetAndGetWork() {
-        XBoard.Grid grid = new XBoard.Grid();
-        grid.set(new XBoard.Cell(2,3,4));
-        grid.set(new XBoard.Cell(3,4,5));
+        Board.Grid grid = new Board.Grid();
+        grid.set(new Board.Cell(2,3,4));
+        grid.set(new Board.Cell(3,4,5));
 
         assertEquals(4, grid.get(2,3).value);
         assertEquals(5, grid.get(3,4).value);
